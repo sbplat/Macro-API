c1530e4c6ff9cdc54e27844d6c0f4bc1e3566bd2
really-noob <yoloeric213@gmail.com>
2022-10-14 20:35:46 -0400

    Update headers and add warning

--- src/platform.h
+++ src/platform.h
@@ -6,9 +6,13 @@
 #if defined(_MACRO_WIN32)
     #include <windows.h>
 #elif defined(_MACRO_COCOA)
-    #error "Unimplemented platform"
+    #warning "Macro API for Cocoa is still WIP"
+    #include <CoreFoundation/CoreFoundation.h>
+    #include <ApplicationServices/ApplicationServices.h>
 #elif defined(_MACRO_X11)
-    #error "Unimplemented platform"
+    #warning "Macro API for X11 is still WIP"
+    #include <X11/Xlib.h>
+    #include <X11/Xutil.h>
 #else
     #error "Unknown platform"
 #endif
