02b2350695362f4798854389f2b727fbb7c4b819
sbplat <71648843+sbplat@users.noreply.github.com>
2022-10-16 17:32:32 -0400

    Use regex to extract the enum keys

--- tools/key_to_string.py
+++ tools/key_to_string.py
@@ -1,24 +1,22 @@
-enum = """
-BACKSPACE, TAB,
-CLEAR, ENTER,
-SHIFT, CTRL, ALT, PAUSE, CAPS_LOCK,
-ESCAPE,
-SPACE, PAGE_UP, PAGE_DOWN, END, HOME, LEFT_ARROW, UP_ARROW, RIGHT_ARROW, DOWN_ARROW, SELECT, PRINT, EXECUTE, PRINT_SCREEN, INSERT, DELETE, HELP,
-ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE,
-A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
-NUMPAD0, NUMPAD1, NUMPAD2, NUMPAD3, NUMPAD4, NUMPAD5, NUMPAD6, NUMPAD7, NUMPAD8, NUMPAD9,
-MULTIPLY, ADD, SEPARATOR, SUBTRACT, DECIMAL, DIVIDE,
-F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
-NUM_LOCK, SCROLL_LOCK,
-LSHIFT, RSHIFT, LCTRL, RCTRL, LALT, RALT, LSUPER, RSUPER, MENU,
-COLON, PLUS, COMMA, MINUS, PERIOD, SLASH, TILDE,
-LEFT_BRACKET, BACKSLASH, RIGHT_BRACKET, QUOTE, OEM_8,
-OEM_102,
-"""
+import re
+
+extract_enum_names_re = re.compile(
+    "enum Key {\n"
+    "    // clang-format off\n"
+    "((?:    [A-Z0-9_ ,]+\n)+)"
+    "    // clang-format on\n"
+    "};"
+)
+
+keyboard_h_data = ""
+with open("../include/macro/keyboard.h", "r") as f:
+    keyboard_h_data = f.read()
+
+enum_names = extract_enum_names_re.findall(keyboard_h_data)[0]
 
 values = [
     value
-    for value in enum.replace("\n", "").replace(" ", "").split(",")
+    for value in enum_names.replace("\n", "").replace(" ", "").split(",")
     if len(value) > 0
 ]
 
@@ -44,8 +42,8 @@ code += """\
     // clang-format on
 }
 
-} // namespace Keyboard
-} // namespace Macro
+}  // namespace Keyboard
+}  // namespace Macro
 """
 
 with open("../src/common/keyboard_key_to_string.cpp", "w") as f:
