e3af88374abe8954d98e37c353b768ba66122ccb
sbplat <71648843+sbplat@users.noreply.github.com>
2022-10-15 23:43:27 -0400

    Update misc.cpp

--- src/win32/misc.cpp
+++ src/win32/misc.cpp
@@ -11,6 +11,10 @@ void PreciseSleep(int ms) {
     LARGE_INTEGER end;
     QueryPerformanceFrequency(&frequency);
     QueryPerformanceCounter(&start);
+    if (ms > 150) {
+        // Use the OS sleep for large intervals to avoid wasting CPU cycles.
+        Sleep(ms - 100);
+    }
     do {
         QueryPerformanceCounter(&end);
     } while ((end.QuadPart - start.QuadPart) * 1000 / frequency.QuadPart < ms);
