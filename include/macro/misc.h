a5e50b7d5463f55652161ddd24fe1b822f84afe2
sbplat <71648843+sbplat@users.noreply.github.com>
2022-10-23 12:25:23 -0400

    doc: add documentation base (#34)
    Setup Doxygen configuration file.
    Document `Sleep` and `PreciseSleep` functions to serve as examples.
    Use `doxygen-awesome-css` theme.
    Add GitHub corner to documentation header.
    Update build workflows to also build docs.
    Create `.htmlhintrc` to silence lint errors for Doxygen header and footer files.

--- include/macro/misc.h
+++ include/macro/misc.h
@@ -4,7 +4,24 @@
 namespace Macro {
 namespace Misc {
 
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Block the current thread for a specified amount of time.
+///
+/// This function is a wrapper around std::this_thread::sleep_for.
+///
+/// \param ms The number of milliseconds to block the current thread for.
+///////////////////////////////////////////////////////////////////////////////
 void Sleep(int ms);  // common
+
+///////////////////////////////////////////////////////////////////////////////
+/// \brief Precisely block the current thread for a specified amount of time.
+///
+/// This function is very accurate, but also expensive. It utilizes the Sleep
+/// function when the specified time is greater than 50ms and spin waits for
+/// the remaining time. The accuracy is normally within 1ms.
+///
+/// \param ms The number of milliseconds to block the current thread for.
+///////////////////////////////////////////////////////////////////////////////
 void PreciseSleep(int ms);
 
 }  // namespace Misc
