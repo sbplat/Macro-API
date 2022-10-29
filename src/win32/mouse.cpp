8c12c1d43ff73b254f14f57716d247de710d3019
Samuel Loza <starsaminf@gmail.com>
2022-10-29 04:44:13 -0400

    Update example  moveAbsolute and scale

--- src/win32/mouse.cpp
+++ src/win32/mouse.cpp
@@ -17,8 +17,8 @@ void MoveAbsolute(int x, int y) {
     // (65535, 65535) is the bottom right corner
     INPUT input = {0};
     input.type = INPUT_MOUSE;
-    input.mi.dx = round(x * 65535 / GetDeviceCaps(GetDC(NULL), HORZRES));
-    input.mi.dy = round(y * 65535 / GetDeviceCaps(GetDC(NULL), VERTRES));
+    input.mi.dx = round(x * 65535.0 / GetDeviceCaps(GetDC(NULL), HORZRES));
+    input.mi.dy = round(y * 65535.0 / GetDeviceCaps(GetDC(NULL), VERTRES));
     input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
     SendInput(1, &input, sizeof(INPUT));
 }
