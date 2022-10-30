78b5a72cf05531ea088bc957f01dd4f7906135c8
sbplat <71648843+sbplat@users.noreply.github.com>
2022-10-30 18:27:20 -0400

    refactor: use CHECK_EQ and different x, y values

--- test/mouse/mouse.h
+++ test/mouse/mouse.h
@@ -1,13 +1,14 @@
 TEST_CASE("[mouse] move absolute") {
-    int expected_x = 300;
-    int expected_y = 300;
+    int expected_x = 120;
+    int expected_y = 160;
 
     Macro::Mouse::MoveAbsolute(expected_x, expected_y);
     Macro::Mouse::Point actual = Macro::Mouse::GetPosition();
 
-    INFO("Position x: ", actual.x);
-    INFO("Position y: ", actual.y);
+    INFO("Expected: " << expected_x << ", " << expected_y);
+    INFO("Actual: " << actual.x << ", " << actual.y);
 
-    REQUIRE(expected_x == doctest::Approx(actual.x).epsilon(0.01));
-    REQUIRE(expected_y == doctest::Approx(actual.y).epsilon(0.01)); 
+    // Allow for a 1% margin of error due to rounding.
+    CHECK_EQ(expected_x, doctest::Approx(actual.x).epsilon(0.01));
+    CHECK_EQ(expected_y, doctest::Approx(actual.y).epsilon(0.01));
 }
